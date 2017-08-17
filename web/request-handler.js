8d5f4ec7f946309730aa613c34f5641fc746cbaf
Joey Li <joeyzaozaoli@gmail.com>
2017-08-16 21:16:19 -0700

    Delete empty directory and remove console.log

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,8 +4,6 @@ var utils = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
 
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
-
   if (req.method === 'GET') {
     if (req.url === '/') {
       utils.serveAssets(res, __dirname + '/public/index.html');
